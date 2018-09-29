9ddf302a25e87492b2c9aa470e059f96f8a7244f
benefic-dispatch-1249 <meganlaynefago@gmail.com>
2018-09-29 20:21:55 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,13 @@
-# Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
+def fizzbuzz(number)
+  return_string =""
+  if number % 3 == 0
+    return_string += "Fizz"
+  end
+  if number % 5 == 0
+    return_string += "Buzz"
+  end
+  if return_string == ""
+    return nil
+  end
+  return_string
+end
